7710a8a3dc908ef396c268742c7bf77f8a894496
Mithikayl <yousufshahzadd@gmail.com>
2022-02-18 10:05:32 +0000

    updates and refactors of previous programs

--- src/stringmanipulation.py
+++ src/stringmanipulation.py
@@ -1,10 +1,10 @@
-import random
+import random, time, os
 
 def main():
     """A program which can manipulate strings"""
     # first we take strings from the user
     inputString = input('What string do you want to manipulate?\n')
-    print(f'Your string is {1}'.format(inputString)) # tell the user their string
+    print(f'Your string is "{inputString}"') # tell the user their string
     maniType = chooseMani()
     prettifyResults(manipulation(maniType, inputString))
     
@@ -23,47 +23,57 @@ class stringEditing:
     
     def randomiseString(self):
         """Randomises the string using random module"""
-        return ''.join(random.choice(self.string) for i in range(len(self.string))) # randomises letters for the length of the string
+        return ''.join(random.choice(self.string) for i in range(len(self.string))) 
 
     def listifyString(self):
-        """Puts a string into a list"""
+        """String manipulation 101"""
         list = [letter for letter in self.string] # iterate through string into lists
         return list
     
     def getConsonants(self):
-        """Gets all consonants in a string"""
+        """Function that grabs all cosonants in a strings"""
         consonants = 'bcdfghjklmnpqrstvwxyz'
-        list = [c for c in self.string if c in consonants]
-        return ''.join(list)
+        list = [v for v in self.string if v in consonants]
+        return ''.join(list)   
 
 def chooseMani():
-    print(f'What kind of manipulation do you want? Reply with the number you want.\n1. String to List\n2. Randomise string letters\n3. Pick out all vowels from string.\n 4. Get consonants from string')
+    print(f'What kind of manipulation do you want? Reply with the number you want.\n1. String to List\n2. Randomise string letters\n3. Pick out all vowels from string.\n4. Get consonants from string')
     while True:
         try:
-            maniChoice = int(input) # check if it is an int
-            if maniChoice > 3: # check if it is an option within our range
+            maniChoice = int(input()) # check if it is an int
+            if maniChoice > 4: # check if it is an option within our range
                 raise ValueError # raise error if not
             else:
                 break
         except ValueError:
             print("That's not a choice.")
+            continue
     return maniChoice
 
 def manipulation(choice, string):
     manipulateString = stringEditing(string)
     if choice == 1:
+        print("You have chosen to make the string into a list.")
+        time.sleep(2)
         return manipulateString.listifyString()
     elif choice == 2:
+        print("You have chosen to randomise the string.")
+        time.sleep(2)
         return manipulateString.randomiseString()
     elif choice == 3:
+        print("You have chosen to get the vowels in the string.")
+        time.sleep(2)
         return manipulateString.getVowels()
     elif choice == 4:
-        return manipulateString.getConsonants()
+        print("You have chosen to get the consonants in the string.")
+        time.sleep(2)
+        return manipulateString.getConsonants()        
     
 
 def prettifyResults(result):
     print(f'The result of your manipulation is:\n{result}') # prettify results
 
 if __name__ == "__main__":
+    os.system('cls')
     main()
 
--- src/urlchecker.py
+++ src/urlchecker.py
@@ -1,31 +1,59 @@
-import os, sys, essentials
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import itertools, time
+# CODE PLAN:
+# structure so that we intake the urls into a list to be iterated through a check
+# can interpret it into a dictionary but we can do that after
+# after that compile url responses into its own list
+# after this, display it in a topdown view. probably a table to show all results 
 
 def main():
-    checkModules()
+    urls = urlIntake()
+    results = urlChecking(urls)
+    displayResults(urls, results)
     pass
 
-def checkModules():
-    while 1:
+def urlIntake():
+    print("Enter how many URLs to test.")
+    while True:
         try:
-            import requests
-            from urllib.request import urlopen, HTTPError, URLError
-        except ImportError:
-            downloadModules = input("You do not have these modules. Would you like to download them? (Y/N)")
-            if downloadModules.lower() == "y":
-                os.system('pip install --user requests urllib3')
-            else:
-                return
-        else:
+            urlCount = int(input())
+        except ValueError:
+            print("Not a valid number.")
             continue
+        else:
+            break
+    urllist = []
+    for i in range(urlCount):
+        url = input("URL: ")
+        urllist.append(url)
+    return urllist
 
-def checkURL(url):
-    try:
-        urlopen(url)
-        return True
-    except HTTPError:
-        return False
-    except URLError:
-        return False
+def urlChecking(urllist):
+    resultList = []
+    for i in urllist:
+        try:
+            urlopen(i)
+        except HTTPError:
+            resultList.append()
+        except:
+            resultList.append(0)
+        else:
+            resultList.append(1)
+    return resultList
 
-if __name__ == '__main__':
+def displayResults(urllist, resultlist):
+    fullResults = []
+    for i in resultlist:
+        if i == 0:
+            fullResults.append("Unavailable")
+        if i == 1:
+            fullResults.append("Available")
+    print("\nRESULTS\n----------------------------------------")
+    time.sleep(2)
+    for(i, x) in zip(urllist, fullResults):
+        print(f"URL: {i}, Status: {x}")
+        time.sleep(1)
+
+if __name__ == "__main__":
     main()
